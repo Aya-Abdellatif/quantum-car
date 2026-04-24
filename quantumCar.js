0eebe014d86f1854b5154b086dbb3ccd4aea5918
Aya Abdellatif <ayaabdllatif112@gmail.com>
2026-04-24 10:41:48 +0300

    Implement Engine

--- quantumCar.js
+++ quantumCar.js
@@ -1,4 +1,19 @@
-class Engine{}
+class Engine{
+    constructor(){
+        this.engineSpeed = 0;
+    }
+    increase(){
+        this.engineSpeed++;
+        console.log(`Engine speed increased to ${this.engineSpeed}`);
+    }
+    decrease(){
+        this.engineSpeed--;
+        console.log(`Engine speed decreased to ${this.engineSpeed}`);
+    }
+    notifySpeedChange(carSpeed){
+        console.log(`Car speed is now ${carSpeed}`);
+    }
+}
 class GasEngine extends Engine {}
 class ElectricEngine extends Engine {}
 class HybridEngine extends Engine {}
